6446b2441ac894be274aa181f716eeb75fba12ba
Manfred Scheiner <manfred@scheinem.com>
2012-11-03 17:55:11 +0100

    Added team-page and fixed Google Analytics

--- scripts/scheiner.js
+++ scripts/scheiner.js
@@ -15,19 +15,9 @@
                 loadTweets();
             };
         }
-    	
-	var _gaq = _gaq || [];
-  	_gaq.push(['_setAccount', 'UA-33475703-1']);
-  	_gaq.push(['_setDomainName', 'it-scheiner.at']);
-  	_gaq.push(['_trackPageview']);
-	var ga = document.createElement('script'); 
-	ga.type = 'text/javascript'; 
-	ga.async = true;
-    	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 
 	var s = document.getElementsByTagName('script')[0]; 
 	s.parentNode.insertBefore(jq, s);
-	s.parentNode.insertBefore(ga, s);
 
     //setInterval('changeReferences()', 5000);
 })();
